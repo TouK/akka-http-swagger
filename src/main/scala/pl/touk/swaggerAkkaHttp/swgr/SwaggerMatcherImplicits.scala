be260065242d13d80d29454e60a1018fb573b8fe
Michał Łępicki <mlc@touk.pl>
2015-08-31 15:48:02 +0200

    an sbt plugin changed license a bit. I won't fight it

--- src/main/scala/pl/touk/swaggerAkkaHttp/swgr/SwaggerMatcherImplicits.scala
+++ src/main/scala/pl/touk/swaggerAkkaHttp/swgr/SwaggerMatcherImplicits.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015 [TouK sp. z o.o. s.k.a](http://www.touk.pl/)
+ * Copyright 2015 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
